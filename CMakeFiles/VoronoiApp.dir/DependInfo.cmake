4d19cdb3bd4ad418b91758e3d129039920da8250
Elies <elies.billotta@gmail.com>
2023-04-21 09:05:39 +0200

    init

---
+++ CMakeFiles/VoronoiApp.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/eniem/IMAC/S2/Voronoi_delaunay/src/SDL2_gfxPrimitives.c" "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.o" "gcc" "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.o.d"
+  "/home/eniem/IMAC/S2/Voronoi_delaunay/src/application_ui.cpp" "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.o" "gcc" "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.o.d"
+  "/home/eniem/IMAC/S2/Voronoi_delaunay/src/main.cpp" "CMakeFiles/VoronoiApp.dir/src/main.cpp.o" "gcc" "CMakeFiles/VoronoiApp.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
