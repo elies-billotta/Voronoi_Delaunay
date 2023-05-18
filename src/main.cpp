2b7232edb888b35aab9533c75faa2ba2f805a1c5
elies-billotta <elies.billotta@edu.univ-eiffel.fr>
2023-05-18 19:57:07 +0200

    delaunay c'est bon ?

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,25 @@ struct Coords
     {
         return x == other.x and y == other.y;
     }
+
 };
 
 struct Segment
 {
     Coords p1, p2;
+
+    bool operator==(const Segment& other)
+    {
+        return p1 == other.p1 && p2 == other.p2;
+    }
+
 };
 
 struct Triangle
 {
     Coords p1, p2, p3;
     bool complet=false;
+
 };
 
 struct Application
@@ -35,14 +43,15 @@ struct Application
     Coords focus{100, 100};
 
     std::vector<Coords> points;
+    std::vector<Segment> segments;
     std::vector<Triangle> triangles;
 };
 
-bool compareCoords(Coords point1, Coords point2)
+bool compareCoords(const Coords p1, const Coords p2)
 {
-    if (point1.y == point2.y)
-        return point1.x < point2.x;
-    return point1.y < point2.y;
+    if (p1.y == p2.y)
+        return p1.x < p2.x;
+    return p1.y < p2.y;
 }
 
 void drawPoints(SDL_Renderer *renderer, const std::vector<Coords> &points)
@@ -55,30 +64,22 @@ void drawPoints(SDL_Renderer *renderer, const std::vector<Coords> &points)
 
 void drawSegments(SDL_Renderer *renderer, const std::vector<Segment> &segments)
 {
-    for (std::size_t i = 0; i < segments.size(); i++)
+    for (const auto &segment : segments)
     {
-        lineRGBA(
-            renderer,
-            segments[i].p1.x, segments[i].p1.y,
-            segments[i].p2.x, segments[i].p2.y,
-            240, 240, 20, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawLine(renderer, segment.p1.x, segment.p1.y, segment.p2.x, segment.p2.y);
     }
 }
 
 void drawTriangles(SDL_Renderer *renderer, const std::vector<Triangle> &triangles)
 {
-    for (std::size_t i = 0; i < triangles.size(); i++)
+    for (const auto &triangle : triangles)
     {
-        const Triangle& t = triangles[i];
-        trigonRGBA(
-            renderer,
-            t.p1.x, t.p1.y,
-            t.p2.x, t.p2.y,
-            t.p3.x, t.p3.y,
-            0, 240, 160, SDL_ALPHA_OPAQUE
-        );
+        SDL_RenderDrawLine(renderer, triangle.p1.x, triangle.p1.y, triangle.p2.x, triangle.p2.y);
+        SDL_RenderDrawLine(renderer, triangle.p2.x, triangle.p2.y, triangle.p3.x, triangle.p3.y);
+        SDL_RenderDrawLine(renderer, triangle.p3.x, triangle.p3.y, triangle.p1.x, triangle.p1.y);
     }
 }
+
 
 void draw(SDL_Renderer *renderer, const Application &app)
 {
@@ -87,6 +88,7 @@ void draw(SDL_Renderer *renderer, const Application &app)
     SDL_GetRendererOutputSize(renderer, &width, &height);
 
     drawPoints(renderer, app.points);
+    drawSegments(renderer, app.segments);
     drawTriangles(renderer, app.triangles);
 }
 
@@ -155,10 +157,11 @@ bool CircumCircle(
     return ((drsqr - *rsqr) <= EPSILON ? true : false);
 }
 
+
 void construitVoronoi(Application &app)
 {
-    
 }
+
 
 bool handleEvent(Application &app)
 {
@@ -195,6 +198,74 @@ bool handleEvent(Application &app)
     return true;
 }
 
+// Fonction de comparaison pour trier les points selon l'axe x
+bool comparePointsX(const Coords& p1, const Coords& p2)
+{
+    return p1.x < p2.x;
+}
+
+void delaunayTriangulation(std::vector<Coords>& points, std::vector<Triangle>& triangles)
+{
+    // Trier les points selon leur coordonnée x
+    std::sort(points.begin(), points.end(), comparePointsX);
+
+    // Vider la liste existante de triangles
+    triangles.clear();
+
+    // Créer un très grand triangle
+    Coords p1{-1000, -1000};
+    Coords p2{500, 3000};
+    Coords p3{1500, -1000};
+    triangles.push_back({p1, p2, p3});
+
+    // Pour chaque point P du repère
+    for (const auto& point : points)
+    {
+        std::vector<Segment> LS;
+
+        // Pour chaque triangle T déjà créé
+        for (auto it = triangles.begin(); it != triangles.end();)
+        {
+            const auto& triangle = *it;
+
+            // Vérifier si le cercle circonscrit contient le point P
+            float xc, yc, rsqr;
+            if (CircumCircle(point.x, point.y, triangle.p1.x, triangle.p1.y, triangle.p2.x, triangle.p2.y,
+                             triangle.p3.x, triangle.p3.y, &xc, &yc, &rsqr))
+            {
+                // Récupérer les segments du triangle dans LS
+                LS.push_back({triangle.p1, triangle.p2});
+                LS.push_back({triangle.p2, triangle.p3});
+                LS.push_back({triangle.p3, triangle.p1});
+
+                // Enlever le triangle de la liste
+                it = triangles.erase(it);
+            }
+            else
+            {
+                ++it;
+            }
+        }
+
+        std::vector<Segment> uniqueSegments;
+
+        // Supprimer les doublons dans LS
+        for (const auto& segment : LS)
+        {
+            if (std::find(uniqueSegments.begin(), uniqueSegments.end(), segment) == uniqueSegments.end())
+            {
+                uniqueSegments.push_back(segment);
+            }
+        }
+
+        // Créer de nouveaux triangles avec les segments et le point P
+        for (const auto& segment : uniqueSegments)
+        {
+            triangles.push_back({segment.p1, segment.p2, point});
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     SDL_Window *gWindow;
@@ -225,8 +296,9 @@ int main(int argc, char **argv)
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // DESSIN
+        // DESSIN FRAME
         draw(renderer, app);
+        delaunayTriangulation(app.points, app.triangles);
 
         // VALIDATION FRAME
         SDL_RenderPresent(renderer);
